fd819a57ebfafc45731cf41e5643a53282f7068f
decentragri <decentragri@gmail.com>
2025-05-25 23:59:26 +0800

    added plantai route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,12 +4,14 @@ import Auth from "./auth.routes";
 import Insight from "./insight.routes";
 import OnChain from "./onchain.routes";
 import Weather from "./weather.routes";
+import PlantAI from "./plant-ai.routes";
 
 
 const routes = (app: any): void => {
     [
         Auth,
         SoilAi,
+        PlantAI,
         Insight,
         OnChain,
         Weather
