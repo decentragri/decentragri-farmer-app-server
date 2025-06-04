ff4ea9f23d9081fdde6ebb52396fb88c86b364b3
decentragri <decentragri@gmail.com>
2025-06-04 15:47:29 +0800

    feat: add optional location property to farmerCreateFarmSchema

--- src/farmer.services/farmer.schema.ts
+++ src/farmer.services/farmer.schema.ts
@@ -7,7 +7,11 @@ export const farmerCreateFarmSchema = {
         farmName: t.String(),
         cropType: t.String(), // List of crop types planted in the farm
         description: t.Optional(t.String()),
-        image: t.Optional(t.String()) // Optional image URL for the farm
+        image: t.Optional(t.String()), // Optional image URL for the farm
+        location: t.Optional(t.Object({
+            lat: t.Number(), // Latitude of the farm location
+            lng: t.Number() // Longitude of the farm location
+        }))
     })
 };
 
